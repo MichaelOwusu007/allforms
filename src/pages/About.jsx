7f05f5780d5fa985657c303cf010726fc0d77d4e
MichaelOwusu <nanayawwiafe007@gmail.com>
2024-02-22 02:02:45 +0000

    allforms

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -42,14 +42,14 @@ function AboutPage() {
                             </div>
                           </div>
                           <div className='  md:w-[350px] rounded-xl mx-auto my-20 bg-white relative w-[300px] sm:w-[350px] h-[370px] sm:h-[420px] flex items-center justify-center'>
-                            <div className='absolute top-[-20%] sm:w-[300px] w-[250px] inset-0 m-auto '>
-                            <img src="/prudence.jpg" alt="" className=' rounded-xl' />
+                            <div className='absolute top-[-20%] sm:w-[310px] w-[250px] inset-0 m-auto '>
+                            <img src="/ridwan.jpg" alt="" className=' rounded-xl  w-[350px] h-[340px] sm:w-[px] sm:h-[400px]' />
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam velit nulla animi</p>
                             </div>
                           </div>
                           <div className='  md:w-[350px] rounded-xl mx-auto my-20 bg-white relative w-[300px] sm:w-[350px] h-[370px] sm:h-[420px] flex items-center justify-center'>
                             <div className='absolute top-[-20%] sm:w-[300px] w-[250px] inset-0 m-auto '>
-                            <img src="/ridwan.jpg" alt="" className=' rounded-xl  w-[360px] h-[340px] sm:w-[] sm:h-[400px] ' />
+                            <img src="/prudence.jpg" alt="" className=' rounded-xl  ' />
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ipsam velit nulla animi</p>
                             </div>
                           </div>
